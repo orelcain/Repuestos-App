571c2b1aea5e23bdb327e74142579cd124dc6ca4
orelcain <orelcain@hotmail.com>
2025-12-20 20:26:08 -0300

    Updates

--- assets/PDFMarkerEditor-BhXQAiS0.js
+++ assets/PDFMarkerEditor-BhXQAiS0.js
@@ -1,4 +1,4 @@
-import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-Bp6xRilr.js";import{p as me,Z as _e}from"./pdf-DMOtFlmx.js";/**
+import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-CE0whszE.js";import{p as me,Z as _e}from"./pdf-DYVUjXSU.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
