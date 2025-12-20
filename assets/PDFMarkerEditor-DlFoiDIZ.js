2c8e24874a5a3d342f76a1465679e878e9a56d0d
orelcain <orelcain@hotmail.com>
2025-12-20 20:42:09 -0300

    Updates

--- assets/PDFMarkerEditor-DlFoiDIZ.js
+++ assets/PDFMarkerEditor-DlFoiDIZ.js
@@ -1,4 +1,4 @@
-import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-CE0whszE.js";import{p as me,Z as _e}from"./pdf-DYVUjXSU.js";/**
+import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-BRVIgjIY.js";import{p as me,Z as _e}from"./pdf-qJW9oGvZ.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/PDFViewer-D9QVRcxg.js
+++ assets/PDFViewer-D9QVRcxg.js
@@ -1,4 +1,4 @@
-import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-CE0whszE.js";import{p as ke,Z as Te}from"./pdf-DYVUjXSU.js";/**
+import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-BRVIgjIY.js";import{p as ke,Z as Te}from"./pdf-qJW9oGvZ.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
