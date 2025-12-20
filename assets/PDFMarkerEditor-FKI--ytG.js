c02c9bd78d8751e4df5b7a5d3a96850b8dd7b113
orelcain <orelcain@hotmail.com>
2025-12-20 16:50:52 -0300

    Updates

--- assets/PDFMarkerEditor-FKI--ytG.js
+++ assets/PDFMarkerEditor-FKI--ytG.js
@@ -1,4 +1,4 @@
-import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-CV6_N86D.js";import{p as me,Z as _e}from"./pdf-CeX9Jy5g.js";/**
+import{c as Y,r,j as t,L as ue,X as qe,S as V,C as he,a as xe,Z as Ve,d as Ze}from"./index-BY7MiRZ9.js";import{p as me,Z as _e}from"./pdf-BslOKvCJ.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/PDFViewer-B3jhwUqZ.js
+++ assets/PDFViewer-B3jhwUqZ.js
@@ -1,4 +1,4 @@
-import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-CV6_N86D.js";import{p as ke,Z as Te}from"./pdf-CeX9Jy5g.js";/**
+import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-BY7MiRZ9.js";import{p as ke,Z as Te}from"./pdf-BslOKvCJ.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
