33255f544334904248723fe7e8f67e8ad6a30363
orelcain <orelcain@hotmail.com>
2025-12-21 18:05:51 -0300

    Updates

--- assets/PDFMarkerEditor-BuadDzBn.js
+++ assets/PDFMarkerEditor-BuadDzBn.js
@@ -1,4 +1,4 @@
-import{c as A,r as o,p as Ee,j as t,L as Pe,X as st,S as se,C as Me,a as Re,U as nt,Z as at,f as rt,g as ot}from"./index-DgubwuNS.js";import{Z as lt}from"./zoom-out-BxRSnKpq.js";/**
+import{c as A,r as o,p as Ee,j as t,L as Pe,X as st,S as se,C as Me,a as Re,U as nt,Z as at,f as rt,g as ot}from"./index-CsoN5sqf.js";import{Z as lt}from"./zoom-out-BizDaoTG.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/zoom-out-BizDaoTG.js
+++ assets/zoom-out-BizDaoTG.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DgubwuNS.js";/**
+import{c as e}from"./index-CsoN5sqf.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
