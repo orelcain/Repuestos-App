4b53c94202c6c71d2efa4f991374a4f12e66343a
orelcain <orelcain@hotmail.com>
2025-12-20 21:11:57 -0300

    Updates

--- assets/PDFViewer-CncWdaWo.js
+++ assets/PDFViewer-CncWdaWo.js
@@ -1,4 +1,4 @@
-import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-Ckajjpof.js";import{p as ke,Z as Te}from"./pdf-W6_DUl8t.js";/**
+import{c as Y,r as a,j as e,L as je,C as Ne,a as ve,S as V,Z as Ce,b as Oe,X as qe,M as se,T as Se,P as Ae}from"./index-BuG32S80.js";import{p as ke,Z as Te}from"./pdf-Cs59G3AH.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
