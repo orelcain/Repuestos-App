b23ae6e00c192fdb509fbf52e52db2ff30535708
orelcain <orelcain@hotmail.com>
2025-12-21 17:40:10 -0300

    Updates

--- assets/zoom-out-DSQ7QhZm.js
+++ assets/zoom-out-DSQ7QhZm.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CJff8wPD.js";/**
+import{c as e}from"./index-CteTanLS.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
